18fbc87dc1122a619aa705a581a204fdc8a849c6
Danilo Chang <ray2501@gmail.com>
2023-12-15 21:03:47 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded rocksdb 0.3.2 \
-    [list load [file join $dir librocksdb0.3.2.so] rocksdb]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded rocksdb 0.3.2 \
+	    [list load [file join $dir libtcl9rocksdb0.3.2.so] [string totitle rocksdb]]
+} else {
+    package ifneeded rocksdb 0.3.2 \
+	    [list load [file join $dir librocksdb0.3.2.so] [string totitle rocksdb]]
+}
